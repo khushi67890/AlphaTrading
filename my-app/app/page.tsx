05e211057251ee2877b83e887fdbbb6cda88e366
khushi67890 <khushiprajapati3005@gmail.com>
2023-07-26 15:35:13 +0530

    updated page.tsx

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            By{' '}
+            By{'kfkd '}
             <Image
               src="/vercel.svg"
               alt="Vercel Logo"
